7af23178920e9f60128f30343712b9c045867436
jingyi <1849612320@qq.com>
2017-07-13 09:17:15 +0800

    archive

--- 国壳漫步/ViewController.m
+++ 国壳漫步/ViewController.m
@@ -9,14 +9,39 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *webview;
+@property (weak, nonatomic) IBOutlet UIToolbar *back;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *forward;
 
 @end
 
 @implementation ViewController
+- (IBAction)back:(id)sender {
+    [self.webview goBack];
+}
+- (IBAction)forward:(id)sender {
+     [self.webview goForward];
+}
+- (IBAction)reload:(id)sender {
+    [self.webview reload];
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.webview.delegate = self;
+    // 网页内容缩小到适应整个设备屏幕
+    //    self.webView.scalesPageToFit = YES;
+    
+    // 检测各种特殊的字符串：比如电话、网站
+    self.webview.dataDetectorTypes = UIDataDetectorTypeAll;
+
+    [self.webview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://gkmb5.herokuapp.com"]]];
+    
+  
+//    self.webview.scrollView.contentInset = UIEdgeInsetsMake(20, 0, 0, 0);
+//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://gkmb5.herokuapp.com"] options:@{} completionHandler:nil];
 }
 
 
